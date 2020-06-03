db30cbc617295162f15df9ef6031c19daaa7de01
k-k-d <krishnadhakshin2000@gmail.com>
2020-06-04 02:42:54 +0530

    Detailed tests with config file

--- test/test1.js
+++ test/test1.js
@@ -1,34 +1,40 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
-const api = supertest('https://dnypx9w-dev.nbd.betalectic.tech');
+const api = supertest('https://dnypx9w-dev.nbd.betalectic.tech');   // baseurl to send http requests
 
 describe('', () => {
 
     let accessToken, refreshToken, authToken;
 
-    it('Login', (done) =>{
-        api.post('/users/auth/login')
-        .set('Content-Type', 'application/json')
-        .send({
+    it('Login API returns the 3 tokens for authorization', (done) =>{   // 1 - testing login with email api
+        api.post('/users/auth/login')   // post request to the login api
+        .set('Content-Type', 'application/json')    // setting headers
+        .send({ // request payload
             username: "krishna@betalectic.com",
             password: "Krishna@09"
         })
-        .expect(200)
+        .expect(200)    // check status code
         .end((err, res) => {
-            console.log(res.body);
-            ({AccessToken: accessToken, RefreshToken: refreshToken, IdToken: authToken} = res.body.AuthenticationResult);
+            expect(res.body).to.have.property("AuthenticationResult");  // check if response payload has the authentication details
+            expect(res.body.AuthenticationResult).to.have.property("AccessToken");
+            expect(res.body.AuthenticationResult).to.have.property("RefreshToken");
+            expect(res.body.AuthenticationResult).to.have.property("IdToken");
+            ({AccessToken: accessToken, RefreshToken: refreshToken, IdToken: authToken} = res.body.AuthenticationResult);   // load authentication ids
             done();
         })
     });
     
-    it('User Details', (done) => {
-        api.get('/users/account/me')
-        .set('Accept', 'application/json')
+    it('User details returns the correct data', (done) => { // 2 - testing get user api
+        api.get('/users/account/me')        // get request to get user api
+        .set('Accept', 'application/json')  // setting headers
         .set('X-Api-Key', accessToken)
         .set('Authorization', authToken)
-        .expect(200)
+        .expect(200)    // check status code
         .end((err, res) => {
-            console.log(res.body);
+            expect(res.body).to.have.property("full_name"); // check if response payload has relevant properties with expected values
+            expect(res.body.full_name).to.equal("venkata krishna");
+            expect(res.body).to.have.property("email");
+            expect(res.body.email).to.equal("krishna@betalectic.com");
             done();
         })
     });
